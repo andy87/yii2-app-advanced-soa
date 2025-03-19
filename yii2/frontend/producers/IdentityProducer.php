f384cbad5995b4d72351bf480357a3660c8e91db
Andrey and _y87 Kidin <kidin_a@list.ru>
2025-03-19 13:16:21 +0300

    `fix`

--- yii2/frontend/producers/IdentityProducer.php
+++ yii2/frontend/producers/IdentityProducer.php
@@ -2,6 +2,8 @@
 
 namespace yii2\frontend\producers;
 
+use yii2\common\models\Identity;
+use yii\db\ActiveRecordInterface;
 use yii2\common\components\core\BaseProduces;
 
 /**
@@ -9,5 +11,5 @@ use yii2\common\components\core\BaseProduces;
  */
 class IdentityProducer extends BaseProduces
 {
-
+    public ActiveRecordInterface|string $modelClass = Identity::class;
 }
